25fe2b691fed853ede69a0772d7c075c1275d8dc
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-07-25 11:45:01 +0530

    Corrects the hiding of div from display property to opacity through css transitions

--- client/templates/layout/layout.js
+++ client/templates/layout/layout.js
@@ -2,7 +2,7 @@ Template.ApplicationLayout.rendered = function () {
     var content = $(this.find('#content'));
     var content_trigger = this.find('#trigger-content');
     if (content_trigger && content) {
-        if (content.is(":visible")) {
+        if (!content.hasClass('transparent')) {
             content_trigger.src = "/images/controls/speech-bubble-active.png";
         }
     }
@@ -12,13 +12,12 @@ Template.ApplicationLayout.events({
         var content = $(template.find('#content'));
         var trigger = template.find('#trigger-content');
         var imageName = 'speech-bubble';
-        if (content.is(':visible')) {
-            content.fadeOut(300);
+        if (content && trigger) {
+            if (content.hasClass('transparent')) {
+                imageName += '-active';
+            }
+            content.toggleClass('transparent');
+            trigger.src = '/images/controls/' + imageName + '.png';
         }
-        else {
-            content.fadeIn(300);
-            imageName += '-active';
-        }
-        trigger.src = '/images/controls/' + imageName + '.png';
     }
 });
