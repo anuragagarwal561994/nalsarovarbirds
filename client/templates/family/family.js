57cdb978dd7006758e13efc5fa6c60fb89c1e96c
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-07-27 04:37:52 +0530

    Completes family functionality

---
+++ client/templates/family/family.js
@@ -0,0 +1,22 @@
+Template.BirdFamily.rendered = function(){
+    var familyToJump = this.find('#'+this.data.name);
+    if(familyToJump){
+        $("#content").animate({
+            scrollTop: $(familyToJump).offset().top
+        }, 'slow', function () {
+            $("html, body").animate({
+                scrollTop: $(familyToJump).offset().top
+            }, 'slow');
+        });
+
+    }
+};
+Template.BirdFamily.events({
+   'click .birds li': function(event, template){
+       Router.go('bird-details', {}, {
+           query: {
+               bird: $(template.find(event.target)).text().toLowerCase().replace(/ /g, '_')
+           }
+       })
+   }
+});
