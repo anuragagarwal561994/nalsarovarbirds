876733c2b96b43c5b056d7e788f11b41ab32a9ce
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-08-01 16:31:30 +0530

    Makes the menu item to store only 5 options at a time and then to include a more option

--- client/templates/menu/menu_data.js
+++ client/templates/menu/menu_data.js
@@ -1,3 +1,5 @@
+const MENU_BREAK_LIMIT = 6;
+
 menus = {
     'List of Birds': function () {
         var birdLink = {};
@@ -8,8 +10,29 @@ menus = {
     }(),
     'Family Classification': {
         'Grebes Podicipedidae': '/family?name=grebes_podicipedidae',
-        'Whistling ducks, Swans, Geese, and Ducks Anatidae': '/family?name=whistling_ducks_swans_geese_and_ducks_anatidae'
+        'Whistling ducks, Swans, Geese, and Ducks Anatidae': '/family?name=whistling_ducks_swans_geese_and_ducks_anatidae',
     },
     'List of Indian State Birds': '/statebird'
 };
 
+var reduceMenu = function (menu) {
+    var keys = Object.keys(menu);
+    var length = keys.length;
+
+    if(length>MENU_BREAK_LIMIT){
+        menu["More"] = {};
+        for(var i=MENU_BREAK_LIMIT;i<length;i++){
+            menu["More"][keys[i]] = menu[keys[i]];
+            delete menu[keys[i]];
+        }
+    }
+    keys = Object.keys(menu);
+    length = keys.length;
+    for(var i=0;i<length;i++){
+        if(menu[keys[i]] instanceof Object){
+            reduceMenu(menu[keys[i]]);
+        }
+    }
+};
+
+reduceMenu(menus);
