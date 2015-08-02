16e17c029dec4a290b8c5cb00d92eee568480919
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-08-02 16:07:55 +0530

    Adds helper functions

--- client/templates/helpers/helpers.js
+++ client/templates/helpers/helpers.js
@@ -1,5 +1,23 @@
 Template.for.helpers({
-    'array': function () {
+    'array' : function () {
         return _.range(this.from, this.to, this.incr);
     }
 });
+String.prototype.neededSubStrings = function (joinCharacter) {
+    return this.match(/[0-9a-z]+s?/gi).join(joinCharacter);
+};
+String.prototype.toUnderscoreFormat = function () {
+    return this.neededSubStrings('_').toLowerCase();
+};
+String.prototype.toTitleCase = function () {
+    return this.neededSubStrings(' ').replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
+toArray = function (obj) {
+    var returnArray = [];
+    for (var key in obj) {
+        returnArray.push({key : key, value : obj[key]});
+    }
+    return returnArray;
+};
