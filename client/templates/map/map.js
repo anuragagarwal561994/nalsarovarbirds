3f88f3c0c84727a15100de4fcd2a549ad5b9e0f5
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-08-03 08:40:12 +0530

    Links bird's name on the map to the bird details page

--- client/templates/map/map.js
+++ client/templates/map/map.js
@@ -3,3 +3,12 @@ Template.Map.helpers({
         return birds[Session.get('current_bird')].name.toUnderscoreFormat();
     }
 });
+Template.Map.events({
+    'click #birdDetailMap area': function(event, template){
+        Router.go('bird-details', {}, {
+            query: {
+                bird: birds[Session.get('current_bird')].name
+            }
+        });
+    }
+});
